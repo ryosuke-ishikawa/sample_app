edfcf2f663013c970d7099a633663e1209c3afa6
ryosuke <r-ishikawa@manaviism.com>
2017-08-10 18:18:57 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
-  devise_for :users
   root  'static_pages#home'
+  devise_for :users
+  resources :users, only: [:show]
+  
   
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
