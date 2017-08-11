9f32d0e7a756eeb97cfe5c591fef15af3d86e78a
ryosuke <r-ishikawa@manaviism.com>
2017-08-11 10:24:37 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
-  root  'static_pages#home'
-  devise_for :users
-  resources :users, only: [:show]
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   
+  resources :users, only: [:show]
+  root  'static_pages#home'
   
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
