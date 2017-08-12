e12c705a6686e95f80f73f50a1db812913d39ed3
ryosuke <r-ishikawa@manaviism.com>
2017-08-12 15:20:04 +0000

    Finish user follow / unfollow actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ SampleApp::Application.routes.draw do
     :registrations => "registrations"
   }
   
-  resources :users, only: [:show, :index, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users, only: [:show, :index, :destroy] do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
   root  'static_pages#home'
   
   match '/help',    to: 'static_pages#help',    via: 'get'
